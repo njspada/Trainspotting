51021194163daac6c2d50d44ab308b4c7e192091
Dhawal Majithia <dmmajithia@ucdavis.edu>
2020-03-29 17:10:18 -0700

    1. Created functions to reduce the lenght of train_detect function.\n2. Still need to reduce train_detect to less than 30 lines.

--- run_camera.py
+++ run_camera.py
@@ -35,6 +35,15 @@ for row in open(modelPath + "coco_labels.txt"):
 # create lists of objects of interest
 vehicles = range(1,9)
 
+detectionStart = ""
+detectionEnd = ""
+detectList = []
+
+# Specify paths
+outputPath = "/mnt/p1/output/"
+logPath = outputPath + "camera_"
+imgPath = outputPath + "images/"
+
 # gstreamer_pipeline returns a GStreamer pipeline for capturing from the CSI camera
 # Defaults to 1280x720 @ 30fps
 # Flip the image by setting the flip_method (most common values: 0 and 2)
@@ -66,138 +75,69 @@ def gstreamer_pipeline(
         )
     )
 
+def save_image(image_path, timestamp, image):
+    cv2.imwrite(image_path + timestamp + '.jpg', image)
+
+def get_timestamp_iso():
+    return datetime.now().isoformat()
+
+def get_image_from_camera(video_capture):
+    _, frame = cap.read()
+    frame = imutils.resize(frame, height = 300, width=300) #check if this is necessary
+    # prepare the frame for object detection by converting it from
+    # 1) BGR to RGB channel ordering and then
+    # 2) from a NumPy array to PIL image format
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    frame = Image.fromarray(frame)
+    return frame
+
+#def save_detect(image_file_path, timestamp, image, results):
+
+def log_detect_list(log_file_name, detect_list, detection_start, detection_end):
+    entry = ['_'.join(detect_list), detection_start, detection_end]
+    record = '.'.join(entry) + '\n'
+    record = record + "\n"
+    with open(log_file_name, 'a') as the_file:
+        the_file.write(record)
 
 def train_detect():
-
-    # Initialize counters used for determining which frames to save
-    # and keep track of events
     detects = 0 # keep track of train detection frames
-    clear = 0   # keep track of frames without trains, check for false negatives
-    # The intervals are dependent on the framerate. The self-reported FPS is 120
+    nondetects = 0 # keep track of frames without trains, check for false negatives
     FPS = 120
-    # collect every 3 seconds at 21 FPS, check for false positives
-    detectInterval = FPS * 3
-    # collect 1 frame every 30 min at 21 FPS, check false neg
-    clearInterval = FPS * 60 * 30
-    
-    detectionStart = ""
-    detectionEnd = ""
-    detectList = []
-
-    # Specify paths
-    outputPath = "/mnt/p1/output/"
-    logPath = outputPath + "camera_"
-    imgPath = outputPath + "images/"
-
-    # Specify the confidence required
-    conf = 0.35 # boo, trains are hard I guess
-
+    detect_interval = FPS * 3
+    nondetect_threshold_interval = FPS * 60 * 30
+    required_confidence = 0.35 # boo, trains are hard I guess
     cap = cv2.VideoCapture(gstreamer_pipeline(), cv2.CAP_GSTREAMER)
-    if cap.isOpened():
-        #cv2.namedWindow("Train Detect", cv2.WINDOW_AUTOSIZE)
-        while cap.isOpened():
-        #while cv2.getWindowProperty("Train Detect", 0) >= 0:
-
-            # Grab time
-            NOW = datetime.now()
-            timeStamp = NOW.isoformat()
-
-            # Catch frame from camera
-            ret, img = cap.read()
-            frame = imutils.resize(img, height = 300, width=300)
-            orig = frame.copy()
-
-            # prepare the frame for object detection by converting it from
-            # 1) BGR to RGB channel ordering and then
-            # 2) from a NumPy array to PIL image format
-            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            frame = Image.fromarray(frame)
-            
-            # make predictions on the input frame
-            results = model.DetectWithImage(frame, threshold=conf,
-                                            keep_aspect_ratio=True,
-                                            relative_coord=False)
-
-            if any(r.label_id in vehicles for r in results):
-                # Vehicle has been detected
-
-                if detects == 0:
-                    detectionStart = timeStamp
-                    
-                if detects % detectInterval == 0:
-                    # Save frame
-                    fname = imgPath + "detect_" + timeStamp + ".jpg"
-                    cv2.imwrite(fname, img)
-
-                detects += 1
-
-                if clear > 0:
-                    clear = 0
-
-                for r in results:
-                    # we only want to look at detections in our list
-                    if r.label_id in vehicles:
-
-                        # For viewing bounding box
-                        box = r.bounding_box.flatten().astype("int")
-                        (startX, startY, endX, endY) = box
-                        label = labels[r.label_id]
-                        # Add label to detect list if not already there
-                        if label not in detectList:
-                            detectList.append(label)
-
-                        # draw the bounding box and label on the image
-                        cv2.rectangle(orig, (startX, startY), (endX, endY),
-                                      (0, 255, 0), 2)
-                        y = startY - 15 if startY - 15 > 15 else startY + 15
-                        text = "{}: {:.2f}%".format(label, r.score * 100)
-                        cv2.putText(orig, text, (startX, y),
-                                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-                
-            else:
-                # No vehicles detected
-                clear += 1
-
-                if detects > 0:
-                    # detection event is over
-                    detectionEnd = timeStamp
-                    
-                    today = NOW.strftime("%Y-%m-%d")
-                    fname = logPath + today + ".log"
-
-                    # Create log entry
-                    entry = ["_".join(detectList), detectionStart, detectionEnd]
-                    record = ",".join(entry)
-                    record = record + "\n"
-
-                    with open(fname, "a") as the_file:
-                        the_file.write(record)
-
-                    # Clear detection list and reset counter
-                    detectList = []
-                    detects = 0
-
-                if clear == clearInterval:
-                    # Save frame
-                    fname = imgPath + "nondetect_" + timeStamp + ".jpg"
-                    cv2.imwrite(fname, img)
-                    
-                    # Reset counter
-                    clear = 0        
-                    
-
-            # show the output frame and wait for a key press
-            #cv2.imshow("Train Detect", orig)
-            keyCode = cv2.waitKey(1) & 0xFF
-            # Stop the program on the 'q' key
-            if keyCode == ord("q"):
-                break
-
-        cap.release()
-        cv2.destroyAllWindows()
-    else:
-        print("Unable to open camera")
-
+    if not cap.isOpened(): print("Unable to open camera.")
+    while cap.isOpened():
+        timeStamp = get_timestamp_iso()
+        frame = get_image_from_camera(cap)
+        # make predictions on the input frame
+        results = model.DetectWithImage(frame, threshold=required_confidence,
+                                        keep_aspect_ratio=True,
+                                        relative_coord=False)
+        if any(r.label_id in vehicles for r in results):
+            # Vehicle has been detected
+            detectionStart = timeStamp if detects == 0 else detectionStart
+            if detects % detect_interval == 0:
+                save_image(imgPath + 'detect_', timeStamp, img)
+            detects += 1
+            nondetects = 0
+        else: # No vehicles detected
+            nondetects += 1
+            if detects > 0:
+                fname = logPath + NOW.strftime("%Y-%m-%d") + ".log"
+                log_detect_list(fname, detectList, detectionStart, timeStamp)
+                # Clear detection list and reset counter
+                detectList = []
+                detects = 0
+            if nondetects == nondetect_threshold_interval:
+                save_image(imgPath + 'nondetect_', timeStamp, img)
+                nondetects = 0        
+        keyCode = cv2.waitKey(1) & 0xFF
+        if keyCode == ord("q"): break 
+    cap.release()
+    cv2.destroyAllWindows()
 
 if __name__ == "__main__":
     train_detect()
