1c365092eaaa27a365a2feb86d05637e3b2c7af3
Dhawal Majithia <dmmajithia@ucdavis.edu>
2020-07-08 13:35:56 -0700

    minibug

--- scripts/run_camera_v1.1.py
+++ scripts/run_camera_v1.1.py
@@ -63,7 +63,7 @@ def display_image(IMAGE, BOX, LABEL, SCORE, FPS):
 	cv2.putText(IMAGE, 'pm2.5=' + str(pa_data['pm2.5']), (20,20), font, 0.5, (200,255,155), 2, cv2.LINE_AA)
 	met_data = met.get_latest_data()
 	cv2.putText(IMAGE, 'windGust=' + str(met_data['windGust']) + 'mph', (20,40), font, 0.5, (200,255,155), 2, cv2.LINE_AA)
-	cv2.putText(IMAGE, 'wgDir=' + str(met_data['windGustDir'] if met_data['windGustDir'] else 'null'), (20,60), font, 0.5, (200,255,155), 2, cv2.LINE_AA)
+	cv2.putText(IMAGE, 'wgDir=' + str(met_data['windGustDir'] if met_data['windGustDir'] else 'null'), (20,240), font, 0.5, (200,255,155), 2, cv2.LINE_AA)
 	cv2.putText(IMAGE, 'fps=' + str(FPS), (20,80), font, 0.5, (200,255,155), 2, cv2.LINE_AA)
 	cv2.imshow('image', IMAGE)
 
