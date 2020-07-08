c7443f1fec087ddf3ad1a739ac16b91db7518392
Dhawal Majithia <dmmajithia@ucdavis.edu>
2020-07-08 13:39:20 -0700

    not resizing image

--- scripts/run_camera_v1.1.py
+++ scripts/run_camera_v1.1.py
@@ -115,7 +115,7 @@ def loop(STREAM, ENGINE, LABELS, DEBUG):
 
 		# run logic here
 		_, image = STREAM.read()
-		image = imutils.resize(image, height = 300, width=300)
+		#image = imutils.resize(image, height = 300, width=300)
 		image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 		detect_candidate = Image.fromarray(image)
 		detections = ENGINE.detect_with_image(detect_candidate, top_k=3, keep_aspect_ratio=True, relative_coord=False)
