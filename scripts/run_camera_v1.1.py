ec643d9dffde7952b4d9a9e7bae95eab0ebad040
Dhawal Majithia <dmmajithia@ucdavis.edu>
2020-07-14 14:25:52 -0700

    added detection confidence parameter

--- scripts/run_camera_v1.1.py
+++ scripts/run_camera_v1.1.py
@@ -203,7 +203,7 @@ def loop(STREAM, ENGINE, DEBUG, MySQLF, EMPTY_FRAMES, TRACKER, CONF):
 		else:
 			#print('detecting')
 			detections = ENGINE.detect_with_image(Image.fromarray(image), top_k=3, keep_aspect_ratio=True, relative_coord=False)
-			train_detects = [d for d in detections if d.label_id == 6]
+			train_detects = [d for d in detections if d.label_id == 6 and d.score > 30.0]
 			# train_detects = [d for d in train_detects if d.score >= CONF]
 			if len(stationary_trains) > 0:
 				temp = []
