f1f5409053d1b2948be59dda1c379af3688e6de5
Dhawal Majithia <dmmajithia@ucdavis.edu>
2020-07-14 11:38:50 -0700

    minibug

--- scripts/run_camera_v1.1.py
+++ scripts/run_camera_v1.1.py
@@ -85,6 +85,7 @@ def debug(DETECT, BOX, FPS, IMAGE, TIMESTAMP):
 	# coords = dict(zip(['startX', 'startY', 'endX', 'endY'], BOX))
 	# dataline = str(TIMESTAMP) + ', ' + LABELS[DETECT.label_id] + ', conf = ' + str(DETECT.score) + ', coords = ' + str(coords) + '\n'
 	# print(dataline)
+	BOX = list(BOX)
 	display_image(IMAGE, BOX, LABELS[DETECT.label_id], DETECT.score, FPS)
 	if cv2.waitKey(1) & 0xFF == ord('q'):
 		return
