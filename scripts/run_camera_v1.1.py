8c17673c9002f3c7de612c8ac7f6d1ca014c0630
Dhawal Majithia <dmmajithia@ucdavis.edu>
2020-07-15 16:15:33 -0700

    removed some debug info

--- scripts/run_camera_v1.1.py
+++ scripts/run_camera_v1.1.py
@@ -71,7 +71,7 @@ def display_image(IMAGE, BOX, LABEL, SCORE, FPS, TRACKING):
 	# met_data = met.get_latest_data()
 	# cv2.putText(IMAGE, 'windGust=' + str(met_data['windGust']) + 'mph', (20,40), font, 0.5, (200,255,155), 2, cv2.LINE_AA)
 	# cv2.putText(IMAGE, 'wgDir=' + str(met_data['windGustDir'] if met_data['windGustDir'] else 'null'), (20,60), font, 0.5, (200,255,155), 2, cv2.LINE_AA)
-	# cv2.putText(IMAGE, 'fps=' + str(FPS), (20,240), font, 0.5, (200,255,155), 2, cv2.LINE_AA)
+	cv2.putText(IMAGE, 'fps=' + str(FPS), (20,240), font, 0.5, (200,255,155), 2, cv2.LINE_AA)
 	cv2.imshow('Trainspotting', IMAGE)
 
 # for debugging
