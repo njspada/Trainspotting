a6f15149ca81bb5c25fc926f7552de6c8fd8cb00
Dhawal Majithia <dmmajithia@ucdavis.edu>
2020-07-27 12:45:38 -0700

    purple air added channel b to sql

--- scripts/purple_air_sql.py
+++ scripts/purple_air_sql.py
@@ -5,7 +5,8 @@ from mysql.connector import errorcode
 
 cnx = database_config.connection()
 
-s_names = ['datetime', 'pm2.5', 'pm1', 'pm10', 'p0.3', 'p0.5', 'p1', 'p2.5', 'p5', 'p10']
+s_names = ['datetime', 'pm2.5', 'pm1', 'pm10', 'p0.3', 'p0.5', 'p1', 'p2.5', 'p5', 'p10', 
+            'pm2.5_b', 'pm1_b', 'pm10_b', 'p0.3_b', 'p0.5_b', 'p1_b', 'p2.5_b', 'p5_b', 'p10_b']
 name_dict = {pair for pair in enumerate(s_names)}
 
 if cnx:
