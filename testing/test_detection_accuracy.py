f35aad1f6b5cceff6bb423b589b66e6cfaf3d301
Dhawal Majithia <dmmajithia@ucdavis.edu>
2020-07-27 16:23:43 -0700

    added detection test script

---
+++ testing/test_detection_accuracy.py
@@ -0,0 +1,85 @@
+import argparse
+import configparser
+from edgetpu.detection.engine import DetectionEngine
+from edgetpu.utils import dataset_utils
+from PIL import Image
+import cv2
+import numpy as np
+import time
+
+
+def gstreamer_pipeline(
+	capture_width=300,
+	capture_height=300,
+	display_width=300,
+	display_height=300,
+	framerate=21,
+	flip_method=0,
+	):
+	return (
+		"nvarguscamerasrc ! "
+		"video/x-raw(memory:NVMM), "
+		"width=(int)%d, height=(int)%d, "
+		"format=(string)NV12, framerate=(fraction)%d/1 ! "
+		"nvvidconv flip-method=%d ! "
+		"video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+		"videoconvert ! "
+		"video/x-raw, format=(string)BGR ! appsink"
+		% (
+			capture_width,
+			capture_height,
+			framerate,
+			flip_method,
+			display_width,
+			display_height,
+		)
+	)
+
+def loop(STREAM, ENGINE, T, CONF):
+	detected = []
+	startTime = time.time()
+	CONF /= 100
+	while time.time() - startTime <= T:
+		_, image = STREAM.read()
+		detections = ENGINE.detect_with_image(Image.fromarray(image), threshold=CONF, top_k=10, keep_aspect_ratio=True, relative_coord=False)
+		train_detects = [d for d in detections if d.label_id == 6]
+		detected.append(len(train_detects))
+	return detected
+
+
+#if __name__ == "__main__":
+def run_test():
+	PARSER = argparse.ArgumentParser(description='Run detection on trains.')
+	PARSER.add_argument('-m', '--model', action='store', default='/usr/share/edgetpu/examples/models/ssd_mobilenet_v2_coco_quant_postprocess_edgetpu.tflite', help="Path to detection model.")
+	PARSER.add_argument('-l', '--label', action='store', default='/usr/share/edgetpu/examples/models/coco_labels.txt', help="Path to labels text file.")
+	PARSER.add_argument('-W', '--width', type=int, action='store', default=300, help="Capture Width")
+	PARSER.add_argument('-H', '--height', type=int, action='store', default=300, help="Capture Height")
+	PARSER.add_argument('-F', '--fps', action='store', type=int, default=60, help="Capture FPS")
+	PARSER.add_argument('-conf', '--confidence', action='store', type=int, default=20, help="Detection confidence level out of 100.")
+	PARSER.add_argument('-t', '--time', action='store', type=int, default=10, help="Number of seconds to detect")
+
+	ARGS = PARSER.parse_args()
+	# Load the DetectionEngine
+	ENGINE = DetectionEngine(ARGS.model)
+	if not ENGINE:
+		print("Failed to load detection engine.")
+		exit()
+	# Read labels file
+	LABELS = dataset_utils.read_label_file(ARGS.label)
+	if not LABELS:
+		print("Failed to load labels file")
+		exit()
+	# Setup image capture stream
+	STREAM = cv2.VideoCapture(gstreamer_pipeline(capture_width = ARGS.width, capture_height = ARGS.height, display_width = ARGS.width, display_height = ARGS.height, framerate=ARGS.fps), cv2.CAP_GSTREAMER)
+
+	try:
+		if not STREAM.isOpened():
+			STREAM.open()
+		return (loop(STREAM, ENGINE, ARGS.time, ARGS.confidence))
+
+		STREAM.release()
+		cv2.destroyAllWindows()
+	except KeyboardInterrupt:
+		print("Program killed")
+		STREAM.release()
+		cv2.destroyAllWindows()
