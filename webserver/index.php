d90de335980846121b94fdfb07a3661490d1c6c9
Dhawal Majithia <dmmajithia@ucdavis.edu>
2020-08-13 17:43:25 -0700

    updated index.php, added save_da() and save_file() functions. needs testing

--- webserver/index.php
+++ webserver/index.php
@@ -1,34 +1,57 @@
 <?php
 
-// $uploaddir = "/";
-// $uploadfile = $uploaddir . basename( $_FILES['file']['name']);
+function save_da(){
+    $query = "";
+    $filename = $_POST["filename"];
+    $device_id = $_POST["device_id"];
+    $file = $_FILES['file']['tmp_name'];
+    $tablename = $_POST["tablename"];
+    
+    // first save the file
+    save_file($file, "logs/".$device_id.$filename);
 
-// if ($_FILES['file']['error'] == UPLOAD_ERR_OK               //checks for errors
-//               && is_uploaded_file($_FILES['file']['tmp_name'])) { //checks that file is uploaded
-//                 echo file_get_contents($_FILES['file']['tmp_name']); 
-// }
+    // create field list from file header
+    $header = fgets(fopen($file,'r'));
+    $header_array = explode(",",$header);
+    $header_array[count($header_array)-1] = explode("\n",$header_array[count($headr_array)-1])[0];
+    $header_array = array_map(function($s) {return '`'.$s.'`'; }, $header_array);
+    $header = implode(',',$header_array);
 
-//$file = file('test_da.csv');
-$file = $_FILES['file']['tmp_name'];
-$mysqli = new mysqli("localhost", "dhawal", "april+1Hitmonlee", "trainspotting");
-if ($mysqli->connect_errno) {
-     echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
-     }
-     echo $mysqli->host_info . "\n";
-$query = <<<eof
-    LOAD DATA LOCAL INFILE '$file'
-     INTO TABLE test_da
-     FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"'
-     LINES TERMINATED BY '\n'
-    (f0,f1,f2,f3)
-eof;
+    // create sql query to insert file
+    $query = <<<eof
+        LOAD DATA LOCAL INFILE '$file'
+         INTO TABLE '$tablename'
+         FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"'
+         LINES TERMINATED BY '\n'
+         IGNORE 1 LINES
+         ($header)
+    eof; 
 
-if($mysqli->query($query) === TRUE){
+    $mysqli = new mysqli("localhost", "dhawal", "april+1Hitmonlee", "trainspotting");
+    if($mysqli->connect_errno) {
+        echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+    }
+
+    if($mysqli->query($query) === TRUE){
         echo "successfully inserted";
-}
-else{
+    }
+    else{
         echo "there was an error\n";
         echo $mysqli->error;
+    }
+}
+
+function save_file($source, $destination){
+    move_uploaded_file($source, $destination);
+}
+
+if(isset($_POST["type"])){
+    if($_POST["type"] == "image"){
+        save_file($_FILES['file']['tmp_name'], "images/".$_POST['device_id'].$_POST['filename']);
+    }
+    else{
+        save_da();
+    }
 }
 
 ?>
