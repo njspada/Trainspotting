17604fd0427aeeb5264e8434ed9a4197a8b1241e
Dhawal Majithia <dmmajithia@ucdavis.edu>
2020-08-19 15:22:17 -0700

    moved webserver code to daily_reporting.php

--- webserver/daily_reporting.php
+++ webserver/daily_reporting.php
@@ -1,5 +1,6 @@
 <?php
 
+echo "hiiiiiiii!";
 
 if(isset($_POST["type"])) {
     if($_POST["type"] == "image"){
@@ -25,37 +26,37 @@ function save_da(){
     echo $tablename;
     // first save the file
     // save_file($file, "logs/".$device_id.$filename);
-    // save_file($file, "logs/".$filename);
+    save_file($file, "logs/".$filename);
 
     // create field list from file header
-    $header = fgets(fopen($file,'r'));
-    $header_array = explode(",",$header);
+    // $header = fgets(fopen($file,'r'));
+    // $header_array = explode(",",$header);
 
-    $header_array[count($header_array)-1] = explode("\n",$header_array[count($header_array)-1])[0];
-    $header_array = array_map(function($s) {return '`'.(str_replace("\"", "", $s)).'`'; }, $header_array);
-    $header = implode(',',$header_array);
+    // $header_array[count($header_array)-1] = explode("\n",$header_array[count($header_array)-1])[0];
+    // $header_array = array_map(function($s) {return '`'.(str_replace("\"", "", $s)).'`'; }, $header_array);
+    // $header = implode(',',$header_array);
 
-    // create sql query to insert file
-    $query = "LOAD DATA LOCAL INFILE '$file' ".
-         "INTO TABLE $tablename ".
-         "FIELDS TERMINATED BY ',' ".
-         "LINES TERMINATED BY '\n' ".
-         "IGNORE 1 LINES ".
-         "($header);"; 
-    echo $query;
+    // // create sql query to insert file
+    // $query = "LOAD DATA LOCAL INFILE '$file' ".
+    //      "INTO TABLE $tablename ".
+    //      "FIELDS TERMINATED BY ',' ".
+    //      "LINES TERMINATED BY '\n' ".
+    //      "IGNORE 1 LINES ".
+    //      "($header);"; 
+    // echo $query;
 
-    $mysqli = new mysqli("localhost", "dhawal", "april+1Hitmonlee", "trainspotting");
-    if($mysqli->connect_errno) {
-        echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
-    }
+    // $mysqli = new mysqli("localhost", "dhawal", "april+1Hitmonlee", "trainspotting");
+    // if($mysqli->connect_errno) {
+    //     echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+    // }
 
-    if($mysqli->query($query) === TRUE){
-        echo "successfully inserted";
-    }
-    else{
-        echo "there was an error\n";
-        echo $mysqli->error;
-    }
+    // if($mysqli->query($query) === TRUE){
+    //     echo "successfully inserted";
+    // }
+    // else{
+    //     echo "there was an error\n";
+    //     echo $mysqli->error;
+    // }
 }
 
 ?>
