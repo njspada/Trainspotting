876a556b859ac2248fea2f4050a1f58977f30b22
Dhawal Majithia <dmmajithia@ucdavis.edu>
2020-08-19 15:44:30 -0700

    added run-reporting config

---
+++ scripts/config_run_reporting.R
@@ -0,0 +1,9 @@
+mysql_user = "dhawal"
+mysql_pw = "april+1Hitmonlee"
+mysql_db_trainspotting = "trainspotting"
+mysql_db_weewx = "weewx"
+mysql_host = "localhost"
+
+post_url <- "192.168.10.120"
+out.dir <- "/home/coal/Desktop/output/logs/"
+images_dir <- "/home/coal/Desktop/output/"
