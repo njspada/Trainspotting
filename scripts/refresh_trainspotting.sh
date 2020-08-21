5c0b42cb7f56a94326e5a4c2354ad1c5a8bd215f
Dhawal Majithia <dmmajithia@ucdavis.edu>
2020-08-20 20:16:58 -0700

    added ngrok setup, and run command in run_trainspotting.sh

--- scripts/refresh_trainspotting.sh
+++ scripts/refresh_trainspotting.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
 sudo systemctl stop run_trainspotting
 R CMD BATCH --no-restore run_reporting.R
+sudo systemctl enable run_trainspotting
+sudo systemctl start run_trainspotting
 sudo systemctl reboot
