58a8196b90ec6cc31ccd3b0e7416ff469b8413d4
Dhawal Majithia <dmmajithia@ucdavis.edu>
2020-08-21 15:32:15 -0700

    added ngrok service

--- scripts/run_trainspotting.sh
+++ scripts/run_trainspotting.sh
@@ -2,6 +2,6 @@
 python3 run_camera.py &
 sudo python3 run_purple_air.py &
 sudo /etc/init.d/weewx start &
-sudo systemctl start ngrok.service
-sudo systemctl enable ngrok.service
+sudo systemctl start run_ngrok.service
+sudo systemctl enable run_ngrok.service
 wait
