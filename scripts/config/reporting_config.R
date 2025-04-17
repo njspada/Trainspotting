aef0f80a904a817e6f9b6c7a165d72eaaf2340af
njspada <62449238+njspada@users.noreply.github.com>
2025-04-16 20:38:17 -0700

    Update reporting_config.R
    Removed usernames, passwords, ip addresses, etc.

--- scripts/config/reporting_config.R
+++ scripts/config/reporting_config.R
@@ -1,23 +1,23 @@
-mysql_user = "dhawal"
-mysql_pw = "april+1Hitmonlee"
+mysql_user = "johndoe"
+mysql_pw = "password"
 mysql_db_trainspotting = "trainspotting"
 mysql_db_weewx = "weewx"
 mysql_host = "localhost"
 
-output_path="/media/coal/sickboyT5/post_covid/"
-post_url="http://100.22.13.192/daily_reporting.php"
+output_path="/path/to/media/"
+post_url="http://ipaddress/server.php"
 dir_logs=paste0(output_path,"logs/")
 dir_images=paste0(output_path,"images/")
 device_id=0
 
-mysql_user = "dhawal"
-mysql_pw = "april+1Hitmonlee"
+mysql_user = "johndoe"
+mysql_pw = "password"
 mysql_db_trainspotting = "trainspotting"
 mysql_db_weewx = "weewx"
 mysql_host = "localhost"
 
 output_path="/media/jetsonssd/trainspotting/"
-post_url="http://100.22.13.192/daily_reporting.php"
+post_url="http://ipaddress/server.php"
 dir_logs=paste0(output_path,"logs/")
 dir_images=paste0(output_path,"images/")
 device_id=0
