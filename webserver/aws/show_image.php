28b7a2af430b3e594249fe180e977acbc9417587
njspada <62449238+njspada@users.noreply.github.com>
2025-04-16 20:47:03 -0700

    Delete webserver/aws directory
    Private information

--- webserver/aws/show_image.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$images_dir = "/home/bitnami/output/images/";
-$filename = $_GET['filename'];
-header('Content-Type: image/jpeg');
-header('Content-Length: ' . filesize($images_dir.$filename));
-echo file_get_contents($images_dir.$filename);
-
-?>
